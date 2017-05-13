6019cd8e24d4caeb480b0b7fb2ff3051de3a27ae
hireaders <ulysses3241@163.com>
2017-05-14 02:15:16 +0800

    2.8简化计算，输出有问题

---
+++ Arithmetic2.8/Arithmetic/Interface.cpp
@@ -0,0 +1,86 @@
+#include "stdafx.h"
+#include "Interface.h"
+#include "Language.h"
+#include<iostream>
+#include<cstring>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int Interface::first()
+{
+	cout << "***********************************************" << endl;
+	cout << "Please choose the language:" << endl;
+
+	languagelist();
+	l=initlanguage();
+	cout << *l;
+	cin >> equation_num;
+	cout << "***********************************************" << endl;
+	return equation_num;
+}
+void Interface::mid(int answer,double result)
+{
+	char ch = getchar();
+	if (answer == result)
+	{
+		correct += 1;
+		cout << *(l + 1);
+	}
+	else if ((answer != result) && (ch != 'e'))
+	{
+		wrong += 1;
+		cout << *(l + 2) << result;
+	}
+	if (ch == 'e')
+	{
+		finally();
+	}
+}
+void Interface::finally()
+{
+	cout << "***********************************************" << endl;
+	cout << *(l+3) << correct << *(l+5) << *(l + 4) << wrong << *(l + 5) << endl;
+	cout << "***********************************************" << endl;
+}
+
+string *Interface::initlanguage()
+{
+	ifstream in;
+
+	char name[100] = { 0 }; //合并后文件路径 
+	char lan_name[20] = { 0 };
+	string *p;
+	int i = 0;
+	int k = 0;
+
+	cin.get(lan_name, 20);//获取输入的字符 
+	strcat_s(name, "language\\");//拼接 
+	strcat_s(name, lan_name);
+	strcat_s(name, ".txt");
+	//cout<<name<<endl;
+	in.open(name, ios::in);            //打开为name的txt 
+	while (in.getline(strp, sizeof(strp), '\n'))
+	{
+		lan[k] = strp;
+		k++;
+	}
+	p = lan;
+	return p;
+}
+
+void Interface::languagelist()
+{
+	fstream file;
+	file.open("language\\languagelist.txt", ios::in);
+
+	int num = 1;
+	char language[20];
+	while (file.getline(language, 20))
+	{
+		cout << num << ". " << language << endl;
+		num++;
+	}
+	file.close();
+	cout << "Please input language name: ";
+}
---
+++ Arithmetic2.8/Arithmetic/equation.cpp
@@ -0,0 +1,172 @@
+#include "stdafx.h"
+#include "equation.h"
+ 
+#include <time.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+const char c[] = "+-*/";
+
+void equation::initequation()
+{
+	result_0 = 9.9;
+	int k = 0;
+	while (result_0 != (int)result_0)
+	{
+		memset(m, '\0', 10);
+		memset(n, '\0', 10);
+		digit_num = rand() % 3 + 5;              //5-7位运算
+		bracket_num = rand() % 2;
+		if (bracket_num == 1)
+		{
+			bracket_a = rand() % ((digit_num - 1) / 2) + 1;
+			bracket_b = bracket_a + rand() % ((digit_num - 3) / 2) + 1;
+		}
+		for (int i = 0;i < digit_num;i++)
+		{
+			if (bracket_num == 1)
+			{
+				if (i == bracket_a)
+				{
+					n[k] = '(';
+					k++;
+				}
+				if (i == bracket_b)
+				{
+					n[k] = ')';
+					k++;
+				}
+			}
+			
+			m[i] = rand() % 8 + 1;
+			n[k] = c[rand() % 4];
+			k++;
+		}
+		if (bracket_num == 1)
+		{
+            n[digit_num + 1] = '=';
+		}
+		else
+		{
+			n[digit_num - 1] = '=';
+		}
+        result_0 = result();
+	}
+	printf();
+}
+
+void equation::printf() {
+	int k = 0;
+	for (int i = 0;i < digit_num;i++)
+	{
+		if (n[k] == '/')
+			cout << m[i] << "÷";
+		else if (n[k] == '(')
+		{
+			if (n[k + 1] == '/')
+				cout << n[k] << m[i] << "÷";
+			else
+				cout << n[k] << m[i] << n[k + 1];
+			k++;
+		}
+		else if (n[k] == ')')
+		{
+			if (n[k + 1] == '/')
+				cout << m[i] << n[k] << "÷";
+			else
+				cout << m[i] << n[k] << n[k + 1];
+			k++;
+		}
+
+		else
+			cout << m[i] << n[k];
+		k++;
+	}
+	cout << result_0 << endl;
+	//cout  << endl;
+}
+
+double equation::result()  //运算答案 
+{
+
+	sign = m[0]; //载入第一个符号
+	result_0 = low(); //进行计算
+	return result_0;
+}
+
+double equation::low() //加减运算 
+{
+	double result1 = mid(); //计算比加减运算优先级别高的部分
+	while ((sign == '+') || (sign == '-'))
+	{
+		if (sign == '+')
+		{
+			sign = m[k1];
+			k1++;
+			result1 += mid();
+		}
+		else
+		{
+			sign = m[k1];
+			k1++;   
+			result1 -= mid();
+		}
+		if (k1 == digit_num || (sign == ')'&&flag == 0))
+		{
+			sign = n[k2];
+			flag = 1;
+			break;
+		}
+	}
+	return result1;
+}
+
+double equation::mid() //乘除运算 
+{
+	double result2 = high();
+	while ((sign == '*') || (sign == '/'))
+	{
+		if (sign == '*')
+		{
+			sign = m[k1];
+			k1++;    
+			result2 *= high();
+
+		}
+		else
+		{
+			sign = m[k1];               //sign为数字
+			k1++;   
+			result2 /= high();
+		}
+		if (k1 == digit_num || (sign == ')'&&flag == 0))
+		{
+			sign = n[k2];
+			flag = 1;
+			break;
+		}
+	}
+	return result2;
+}
+
+
+double equation::high() //括号运算及数字 
+{
+
+	double result3 = 0;
+	if (n[k2] == '(')          //进入带有括号
+	{
+		k2++;
+		result3 = low();        //递归计算表达式
+		k2++;
+	}
+	if (sign >= 0 && sign <= 9) //实际的数字
+	{
+		result3 = sign;
+		sign = n[k2];
+		k2++;
+	}
+	return result3;
+}
