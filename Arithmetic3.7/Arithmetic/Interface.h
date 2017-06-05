b494512c5bd8b8ee975b872960957bcf3a0a3406
hireaders <ulysses3241@163.com>
2017-06-06 01:33:44 +0800

    简单工厂+random父类

---
+++ Arithmetic3.7/Arithmetic/Interface.h
@@ -0,0 +1,25 @@
+#pragma once
+#include<string>
+using namespace std;
+
+class Interface       //界面
+{
+public:
+	int first();           //开始界面（包括语言）
+	void mid(int, double );      //每一道题的结果
+	void finally();         //统计错题数
+
+private:
+	string *initlanguage();
+	void languagelist();
+
+	int equation_num,
+		correct,    //一共对了
+		wrong;      //一共错了
+	string *l,      //init后语言指针
+	       lan[10];
+	char name[10],
+		 str[100],//内容 
+	     strp[100];
+};
+
